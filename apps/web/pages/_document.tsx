1dd9fd09dfa9e798972ae3c2c1abc06529ab07db
gabrielmeloc22 <gabrielpresb@gmail.com>
2023-04-21 11:51:07 -0300

    first commit

---
+++ apps/web/pages/_document.tsx
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&display=swap"
+        rel="stylesheet"
+      ></link>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
