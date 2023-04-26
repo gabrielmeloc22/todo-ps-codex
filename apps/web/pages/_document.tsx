4aeaa96eb6c99b6bf76edc1b05ea20f8cacce8fe
gabrielmeloc22 <gabrielpresb@gmail.com>
2023-04-25 23:16:54 -0300

    feat(styling): add tailwind scaffolding

--- apps/web/pages/_document.tsx
+++ apps/web/pages/_document.tsx
@@ -9,7 +9,7 @@ export default function Document() {
       <link
         href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&display=swap"
         rel="stylesheet"
-      ></link>
+      />
       <body>
         <Main />
         <NextScript />
