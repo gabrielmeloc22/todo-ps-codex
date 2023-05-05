742032c24a2d3c74f5895a1412af7af7035db694
gabrielmeloc22 <gabrielpresb@gmail.com>
2023-05-04 21:44:27 -0300

    refactor: use tsconfig on web and add import aliases

--- packages/ui/src/components/Button.tsx
+++ packages/ui/src/components/Button.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { VariantProps, cva } from "class-variance-authority";
 
-import { cn } from "../lib/utils";
+import { cn } from "@ui/lib/utils";
 import { Loader2 } from "lucide-react";
 
 const buttonVariants = cva(
