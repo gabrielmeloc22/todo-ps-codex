441823177d7e39a1321b80ee44d8745035c5a72f
PedroCost4 <pedro.costacdc@gmail.com>
2023-05-07 17:46:03 -0300

    "feat create Task refactor and done"

--- apps/server/src/useCases/task/createTask/createTask.controller.ts
+++ apps/server/src/useCases/task/createTask/createTask.controller.ts
@@ -1,22 +1,24 @@
 import { Request, Response } from 'express';
 import  CreateTaskUseCase from './createTask.useCase';
-import { Task } from "@prisma/client"
+import Task  from "../../../models/Task";
 
 class createTaskController {
     constructor(private createTaskUseCase: CreateTaskUseCase) {}
 
     async handle(request: Request, response: Response): Promise<Response> {
-        const { title, authorId, collectionId }: Task = request.body;
-
+        const { title, authorId, content, status, collectionId}: Task = request.body;
+        
         const task = await this.createTaskUseCase.execute(
             {
                 title: title,
                 authorId: authorId,
-                collectionId: collectionId,
+                content: content,
+                status: status,
+                collectionId: collectionId
             }
         );
 
-        return response.status(201).json(task);
+        return response.status(200).json(task);
     }
 }
 
--- apps/server/src/useCases/task/createTask/createTask.useCase.ts
+++ apps/server/src/useCases/task/createTask/createTask.useCase.ts
@@ -1,12 +1,19 @@
-import { Task } from "@prisma/client"
+import Task  from "../../../models/Task";
 import ITaskRepository from "../../../repositories/interfaces/task.repository.interface";
 
 
 class createTaskUseCase {
     constructor(private taskRepository: ITaskRepository) {}
     
-    async execute(data: any) {
-        const task = await this.taskRepository.createTask(data);
+    
+    async execute({title, content, status, authorId, collectionId}: Task) {
+        const task = await this.taskRepository.createTask({
+            title,
+            content,
+            status,
+            authorId,
+            collectionId
+        });
         return task;
     }
 }
