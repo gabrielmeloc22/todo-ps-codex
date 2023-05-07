92cf4754d9f76a51d9e188658d93b482e573c7cb
Bruno Rodrigues Ramos <103748132+BrunoRRamos@users.noreply.github.com>
2023-05-07 15:45:50 -0300

    Feat/user.use case (#13)
    * feat: Models and Controllers structure
    
    * fix: User, Task and Collections user non required
    
    * feat: User repo get(byId, byEmail) and deleteUser
    
    * feat: new schema migration
    
    * feat: getUser and CreateUser useCases

--- apps/server/src/repositories/interfaces/user.repository.interface.ts
+++ apps/server/src/repositories/interfaces/user.repository.interface.ts
@@ -1,11 +1,11 @@
 import User from "../../models/User";
 
 interface IUserRepository { 
-    getUserById(id: number): Promise<User>;
-    getUserByEmail(email: string): Promise<User>;
-    createUser(user: User): Promise<User>;
-    updateUser(user: User): Promise<User>;
-    deleteUser(id: number): Promise<User>;
+    getUserById(id: String): Promise<User | null>;
+    getUserByEmail(email: String): Promise<User | null>;
+    createUser(user: User): Promise<User | null>;
+    updateUser(user: User): Promise<User | null>;
+    deleteUser(id: String): Promise<User | null>;
 }
 
 export default IUserRepository;
