99981382e12fb629d0040b2196722bc2773eb7ea
gabrielmeloc22 <gabrielpresb@gmail.com>
2023-05-11 19:48:39 -0300

    feat(ui): add label component

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,4 @@
 export { Button } from "@ui/components/Button";
 export { Checkbox } from "@ui/components/Checkbox";
 export { Input } from "@ui/components/Input";
+export { Label } from "@ui/components/Label";
