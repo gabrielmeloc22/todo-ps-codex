d45bd95a38b0383aaacb5198f84fdd21f8f36fb4
MisterBeamCs <103748132+BrunoRRamos@users.noreply.github.com>
2023-05-11 22:48:30 -0300

    feat: CollectionsUseCase

--- apps/server/src/useCases/collection/createCollection/createCollection.useCase.ts
+++ apps/server/src/useCases/collection/createCollection/createCollection.useCase.ts
@@ -1,2 +1,19 @@
 import Collection from "../../../models/Collection";
-import I
+import ICollectionRepository from "../../../repositories/interfaces/collection.repository.interface";
+
+class createCollectionUseCase {
+    constructor(private collectionRepository: ICollectionRepository) {}
+
+
+    async execute({title, color, authorId, author, createdAt}: Collection) {
+        const collection = await this.collectionRepository.createCollection({
+            title,
+            color,
+            authorId,
+            author
+        });
+        return collection;
+    }
+}
+
+export default createCollectionUseCase;
