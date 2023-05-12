13c049f1a9ba5f3118d990b3b4c23df0c9be49f3
BrunoRRamos <brbosshd2@gmail.com>
2023-05-11 23:35:30 -0300

    feat: COllections UseCases

--- apps/server/src/useCases/collection/createCollection/createCollection.controller.ts
+++ apps/server/src/useCases/collection/createCollection/createCollection.controller.ts
@@ -3,8 +3,17 @@ import createCollectionUseCase from './createCollection.useCase';
 import Collection from '../../../models/Collection';
 
 class CreateCollectionController {
+    private static instance: CreateCollectionController;
+    
+
     constructor(private createCollectionUseCase: createCollectionUseCase) {}
 
+    public static getInstance(): CreateCollectionController {
+        if (!CreateCollectionController.instance) {
+            CreateCollectionController.instance = new CreateCollectionController(createCollectionUseCase.getInstance());
+        }
+        return CreateCollectionController.instance;
+    }
 
     async handle(request: Request, response: Response): Promise<Response> {
         const { title, color, authorId, tasks }: Collection = request.body;
--- apps/server/src/useCases/collection/createCollection/createCollection.useCase.ts
+++ apps/server/src/useCases/collection/createCollection/createCollection.useCase.ts
@@ -1,8 +1,18 @@
 import Collection from "../../../models/Collection";
+import CollectionRepository from "../../../repositories/implementations/collection.repository";
 import ICollectionRepository from "../../../repositories/interfaces/collection.repository.interface";
 
 class CreateCollectionUseCase {
     constructor(private collectionRepository: ICollectionRepository) {}
+
+    private static instance: CreateCollectionUseCase;
+
+    public static getInstance(): CreateCollectionUseCase {
+        if (!CreateCollectionUseCase.instance) {
+            CreateCollectionUseCase.instance = new CreateCollectionUseCase(CollectionRepository.getInstance());
+        }
+        return CreateCollectionUseCase.instance;
+    }
 
 
     async execute({title, color, authorId, tasks}: Collection) {
