4be12147f4da33b3da7651599dde6f99ab4780e3
gabrielmeloc22 <gabrielpresb@gmail.com>
2023-05-14 01:05:19 -0300

    feat(web): add loginPage

--- apps/web/app/login/lib/useUserLoginMutation.ts
+++ apps/web/app/login/lib/useUserLoginMutation.ts
@@ -1,5 +1,43 @@
-import { useMutation } from "@tanstack/react-query";
+import { MutationFunction, useMutation } from "@tanstack/react-query";
+import { api } from "../../../services/axios";
+import { setCookie } from "cookies-next";
+import { useRouter } from "next/navigation";
+
+type UserLoginRes = {
+  token: string;
+  user: {
+    id: string;
+    name: string;
+    lastName: string;
+    email: string;
+  };
+};
+
+type UserLoginVariables = {
+  email: string;
+  password: string;
+};
+
+const loginUser: MutationFunction<UserLoginRes, UserLoginVariables> = async (variables) => {
+  const { data } = await api.post("user/login", variables);
+  return data;
+};
+
+const setUserCookies = (token: string, id: string) => {
+  setCookie("auth_token", token);
+  setCookie("user_id", id);
+};
 
 export function userUserLoginMutation() {
-  const { mutate } = useMutation({});
+  const router = useRouter();
+
+  const mutation = useMutation({
+    mutationFn: loginUser,
+    onSuccess: (data) => {
+      setUserCookies(data.token, data.user.id);
+      router.push("dashboard");
+    },
+  });
+
+  return mutation;
 }
