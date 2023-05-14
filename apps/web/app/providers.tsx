0198e42ccdfaf990091b9fa2c2c0c6d87528d289
gabrielmeloc22 <gabrielpresb@gmail.com>
2023-05-14 01:04:24 -0300

    feat(web): add auth context

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -3,11 +3,16 @@
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "../services/ReactQuery";
 import { ReactNode } from "react";
+import { AuthContextProvider } from "../context/auth";
 
 interface ProvidersProps {
   children: ReactNode;
 }
 
 export function Providers({ children }: ProvidersProps) {
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthContextProvider>{children}</AuthContextProvider>
+    </QueryClientProvider>
+  );
 }
