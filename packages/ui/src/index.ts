575f490651a2bfbbcf65c1289d34ba4df435bfe8
gabrielmelo22 <gabrielpresb@gmail.com>
2023-05-20 11:34:13 -0300

    feat(ui): add dialogComponent

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,3 +2,12 @@ export { Button } from "@ui/components/Button";
 export { Checkbox } from "@ui/components/Checkbox";
 export { Input } from "@ui/components/Input";
 export { Label } from "@ui/components/Label";
+export {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@ui/components/Dialog";
