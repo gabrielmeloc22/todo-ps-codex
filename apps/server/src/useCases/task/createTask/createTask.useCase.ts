735aaca47b7dfd93b8c98aa829123450ff6e931f
Pedro Costa <pedro.costacdc@gmail.com>
2023-05-20 14:26:27 -0300

    feat: check null date in task

--- apps/server/src/useCases/task/createTask/createTask.useCase.ts
+++ apps/server/src/useCases/task/createTask/createTask.useCase.ts
@@ -1,15 +1,20 @@
-import Task from "../../../models/Task";
+import { Task } from "@prisma/client";
 import ITaskRepository from "../../../repositories/interfaces/task.repository.interface";
+
+type TaskInput = Omit<Task, "id" | "createdAt" | "updatedAt"> & { completionDate: string };
 
 class createTaskUseCase {
     constructor(private taskRepository: ITaskRepository) {}
 
-    private dateToString(stringDate: string):Date {
+    private dateToString(stringDate: string):Date | null {
+        if (stringDate === "") {
+            return null;
+        }
         const date = new Date(stringDate);
         return date;
     }
     
-    async execute({title, content, completionDate, status, authorId, collectionId}: Task) {
+    async execute({title, content, completionDate, status, authorId, collectionId}: TaskInput) {
         const task = await this.taskRepository.createTask({
             title,
             content,
