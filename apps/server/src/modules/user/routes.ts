f42f2f19dc6eb9488d4470e27fd80f780a20425a
BrunoRRamos <brbosshd2@gmail.com>
2023-05-21 08:57:08 -0300

    feat: UpdateUser Route

--- apps/server/src/modules/user/routes.ts
+++ apps/server/src/modules/user/routes.ts
@@ -1,9 +1,11 @@
-import { Router } from "express";
+import { Router, response } from "express";
 import { ensureAuthenticated } from "../../middleware/ensureAuthenticated";
 import createUser from "./controllers/create";
 import deleteUser from "./controllers/delete";
 import getUser from "./controllers/get";
+import updateUser from "./controllers/update"
 import authenticateUser from "./controllers/authenticate";
+
 
 const router = Router();
 
@@ -16,6 +18,9 @@ router
   })
   .post("/login", (request, response) => {
     return authenticateUser.handle(request, response);
+  })
+  .put("/:id", (request, response) => {
+    return updateUser.handle(request, response);
   })
   .delete("/:id", (request, response) => {
     return deleteUser.handle(request, response);
