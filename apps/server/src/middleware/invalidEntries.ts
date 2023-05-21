175379a20cfd14718cbc3a36a081ed81219d2375
Pedro Costa <pedro.costacdc@gmail.com>
2023-05-21 00:16:19 -0300

    refactor: middleware

--- apps/server/src/middleware/invalidEntries.ts
+++ apps/server/src/middleware/invalidEntries.ts
@@ -1,26 +1,22 @@
 import prisma from "./prisma/client";
-import { User } from "@prisma/client";
 
 class InvalidEntries {
-    private static instance: InvalidEntries;
 
-    constructor() {
+    public static async userAlreadyExists(email: string): Promise<void> {
 
-    }
+        const user = await prisma.user.findFirst(
+          { where: {
+             email 
+          } 
+          });
 
-    public static getInstance() {
-        if (!this.instance) {
-            return new InvalidEntries();
-        }
-        return this.instance;
-    }
-
-    public async userAlreadyExists(email: string): Promise<void> {
-        const user = await prisma.user.findFirst({ where: { email } });
         if (user) {
+
           throw new Error("User Already Exists");
+
         }
-      }
+
+    }
 }
 
 export default InvalidEntries;
