a08ac8d7063ff09b9b84efa684f14dd1919130c5
BrunoRRamos <brbosshd2@gmail.com>
2023-05-21 09:02:04 -0300

    fix: FieldData type change to string or number

--- apps/server/src/modules/user/repositories/update.ts
+++ apps/server/src/modules/user/repositories/update.ts
@@ -2,7 +2,7 @@ import { User } from "@prisma/client";
 import prisma from "../../../middleware/prisma/client";
 
 class UpdateUserRepository {
-    static async update(idx: string, fieldName: string, fieldData: any): Promise<User> {
+    static async update(idx: string, fieldName: string, fieldData: string | number): Promise<User> {
         const updatedUser = await prisma.user.update({ 
             where: {
                 id: idx
