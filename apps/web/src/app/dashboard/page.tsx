0d8edd82012bdf351255a06bf7909d2993737007
gabrielmeloc22 <gabrielpresb@gmail.com>
2023-05-24 01:21:57 -0300

    feat(web): add dashboard main page

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -1,9 +1,26 @@
 "use client";
 
-import { useAuth } from "../../context/auth";
+import { Metadata } from "next";
+import { useGetTasksQuery } from "../../../hooks/useGetTasksQuery";
+import { Header } from "./components/Header";
+import { Task } from "./components/Task";
+
+export const metadata: Metadata = {
+  title: "AllDone | Visão Geral",
+};
 
 export default function DashboardPage() {
-  const { name } = useAuth();
+  const { data, isLoading, isSuccess } = useGetTasksQuery();
 
-  return <div>olá, {name}</div>;
+  return (
+    <main className="w-full max-w-[40vw] flex h-screen ml-[8vw] flex-col gap-32 px-10 py-20">
+      <Header />
+      <section className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] grid-flow-row gap-4 mt-10">
+        {isSuccess &&
+          data.map((task) => {
+            return <Task key={task.id} data={task} />;
+          })}
+      </section>
+    </main>
+  );
 }
