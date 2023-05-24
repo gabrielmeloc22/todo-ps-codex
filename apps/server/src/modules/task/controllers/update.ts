7606e30bd73e73e081d6c25ae01349fb6b193d07
Pedro Costa <pedro.costacdc@gmail.com>
2023-05-24 00:03:18 -0300

    feat: authentication update task

--- apps/server/src/modules/task/controllers/update.ts
+++ apps/server/src/modules/task/controllers/update.ts
@@ -1,13 +1,18 @@
 import { Request, Response } from "express";
 import UpdateTaskUseCase from "../useCases/update";
+import CheckUser from "../../../utils/checkUser";
 
 class UpdateTaskController {
 
     static async handle(request: Request, response: Response) {
-        const { id } = request.params;
+        const { authorId, taskId } = request.params;
         const data = request.body;
 
-        const updateTask = await UpdateTaskUseCase.execute(id, data);
+        const tokenUserId = request.headers.userId as string;
+
+        CheckUser.check(tokenUserId, authorId);
+
+        const updateTask = await UpdateTaskUseCase.execute(taskId, data);
 
         return response.status(200).json(updateTask);
         
