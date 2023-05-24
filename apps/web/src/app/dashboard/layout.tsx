28abea8d9a99400a8b5a9e7e54e19a837a24cf29
gabrielmeloc22 <gabrielpresb@gmail.com>
2023-05-24 08:41:51 -0300

    fix(web): fix import and metadata
    fix(web): use optional chaining

--- apps/web/src/app/dashboard/layout.tsx
+++ apps/web/src/app/dashboard/layout.tsx
@@ -1,9 +1,13 @@
+import { Metadata } from "next";
 import { Sidebar } from "./components/Sidebar";
+
+export const metadata: Metadata = {
+  title: "AllDone | Visão Geral",
+};
 
 interface DashboardLayoutProps {
   children: React.ReactNode;
 }
-
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
   return (
     <div className="flex">
--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -1,13 +1,8 @@
 "use client";
 
-import { Metadata } from "next";
 import { useGetTasksQuery } from "../../../hooks/useGetTasksQuery";
 import { Header } from "./components/Header";
 import { Task } from "./components/Task";
-
-export const metadata: Metadata = {
-  title: "AllDone | Visão Geral",
-};
 
 export default function DashboardPage() {
   const { data, isLoading, isSuccess } = useGetTasksQuery();
@@ -16,10 +11,9 @@ export default function DashboardPage() {
     <main className="w-full max-w-[40vw] flex h-screen ml-[8vw] flex-col gap-32 px-10 py-20">
       <Header />
       <section className="grid grid-cols-[repeat(auto-fit,minmax(250px,1fr))] grid-flow-row gap-4 mt-10">
-        {isSuccess &&
-          data.map((task) => {
-            return <Task key={task.id} data={task} />;
-          })}
+        {data?.map((task) => {
+          return <Task key={task.id} data={task} />;
+        })}
       </section>
     </main>
   );
