bac845baa9c0c8be2da84cccc696d2524f94d55c
gabrielmeloc22 <gabrielpresb@gmail.com>
2023-05-25 17:56:28 -0300

    feat(web): add signOut button

--- apps/web/src/app/dashboard/components/Profile.tsx
+++ apps/web/src/app/dashboard/components/Profile.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import { Button } from "ui";
-import { useAuth } from "../../../../components/Auth";
+import { signOut, useAuth } from "@/components/Auth";
 import NextLink from "next/link";
 import NextImage from "next/image";
+import { useRouter } from "next/navigation";
 
 export function Profile() {
   const { lastName, name } = useAuth();
+  const router = useRouter();
 
   return (
     <div className="flex gap-6">
@@ -18,12 +20,19 @@ export function Profile() {
           {name} {lastName}
         </p>
         <div className="mt-2 flex gap-4 leading-none">
-          <NextLink href="profile">
+          <NextLink href="/dashboard/profile">
             <Button variant="link" className="text-zinc-300 font-light p-0 w-fit h-fit">
               Meu perfil
             </Button>
           </NextLink>
-          <Button variant="link" className="text-zinc-300 font-light p-0 w-fit h-fit">
+          <Button
+            variant="link"
+            className="text-zinc-300 font-light p-0 w-fit h-fit"
+            onClick={() => {
+              signOut();
+              router.replace("login");
+            }}
+          >
             Sair
           </Button>
         </div>
