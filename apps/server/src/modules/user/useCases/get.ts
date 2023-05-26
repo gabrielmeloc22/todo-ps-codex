2ba37ef9a79144feb9fef077f35144c0034287ab
PedroCost4 <pedro.costacdc@gmail.com>
2023-05-26 17:49:56 -0300

    feat: user not found

--- apps/server/src/modules/user/useCases/get.ts
+++ apps/server/src/modules/user/useCases/get.ts
@@ -1,12 +1,22 @@
+import { User } from "@prisma/client";
+import HttpError from "../../../utils/HttpError";
 import GetUserRepository from "../repositories/get";
 
-class getUserUseCase {
+class GetUserUseCase {
+
+    static userNull (user: User | null) {
+        if (user === null) {
+            throw new HttpError("User Not found", 404)
+        }
+    }
+
     
     static async execute(id: string) {
-        const user = GetUserRepository.getById(id);
+        const user = await GetUserRepository.getById(id);
+        GetUserUseCase.userNull(user);
         return user;
     }
 
 }
 
-export default getUserUseCase;
+export default GetUserUseCase;
