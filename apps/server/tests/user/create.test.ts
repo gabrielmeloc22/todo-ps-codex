1074140178fbdf43d4ac041e8911c311c6f3f188
BrunoRRamos <brbosshd2@gmail.com>
2023-05-26 19:36:51 -0300

    feat: test example

---
+++ apps/server/tests/user/create.test.ts
@@ -0,0 +1,8 @@
+describe('User Create Controller Test', () => {
+    it('throw num1  + num2', () => {
+        const num1 = 2;
+        const num2 = 2;
+
+        expect(num1 + num2).toEqual(4);
+    });
+});
