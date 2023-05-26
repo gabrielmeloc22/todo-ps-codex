8a0d7d1b00840ea7bee89471de4a49e7af9375e7
Pedro Costa <70077205+PedroCost4@users.noreply.github.com>
2023-05-26 17:55:49 -0300

    Feat/user not found (#46)
    * fix: auth put route
    
    * feat: added photo url in db
    
    * refactor: profile pic name in bd
    
    * refactor: Omit type
    
    * feat: order tasks by date
    
    * feat: user not found

--- apps/server/src/modules/user/useCases/update.ts
+++ apps/server/src/modules/user/useCases/update.ts
@@ -1,5 +1,6 @@
 import { User, Prisma } from "@prisma/client"
 import UpdateUserRepository from "../repositories/update"
+import HttpError from "../../../utils/HttpError";
 
 class UpdateUserUseCase {
     static async excute(id: string, data: Prisma.UserUpdateInput) {
