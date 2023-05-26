ca7258de2a36e3c54b4434a92c11636611b846bd
Pedro Costa <70077205+PedroCost4@users.noreply.github.com>
2023-05-26 17:31:05 -0300

    Feat/order (#45)
    * fix: auth put route
    
    * feat: added photo url in db
    
    * refactor: profile pic name in bd
    
    * refactor: Omit type
    
    * feat: order tasks by date

--- apps/server/src/modules/task/repositories/get.ts
+++ apps/server/src/modules/task/repositories/get.ts
@@ -1,26 +1,28 @@
-import { Task } from "@prisma/client";
+import { Prisma, Task } from "@prisma/client";
 import prisma from "../../../middleware/prisma/client";
 
 class GetTaskRepository {
-
     static async get(id: string): Promise<Task | null> {
         const outputTask = await prisma.task.findUnique({
             where: {
-                id: id
-            }
+                id: id,
+            },
         });
         return outputTask;
     }
 
-    static async getAll(authorId: string): Promise<Task[]> {
+    static async getAll(authorId: string, orderBy: Prisma.SortOrder): Promise<Task[]> {
+
         const allTasks = await prisma.task.findMany({
             where: {
-                authorId: authorId
-            }
-        })
+                authorId: authorId,
+            },
+            orderBy: {
+                completionDate: orderBy || 'asc',
+            },
+        });
         return allTasks;
     }
-
 }
 
 export default GetTaskRepository;
