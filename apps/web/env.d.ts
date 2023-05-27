92dc517e7279425b05490382c9c10c70074ffd2b
gabrielmeloc22 <gabrielpresb@gmail.com>
2023-05-27 00:09:26 -0300

    feat(web): add supabase sdk

--- apps/web/env.d.ts
+++ apps/web/env.d.ts
@@ -1,5 +1,7 @@
 namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_API_URL: string;
+    NEXT_PUBLIC_SUPABASE_PROJECT_URL: string;
+    NEXT_PUBLIC_SUPABASE_PUBLIC_ANON_KEY: string;
   }
 }
