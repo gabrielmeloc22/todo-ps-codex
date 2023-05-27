faa35639671f8b4c3751155a39bef5e862664eda
gabrielmeloc22 <gabrielpresb@gmail.com>
2023-05-27 00:20:55 -0300

    feat(web): use tooltip provider and add styling tweak

--- apps/web/src/app/providers.tsx
+++ apps/web/src/app/providers.tsx
@@ -4,6 +4,7 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode } from "react";
 import { Auth } from "@/components/Auth";
 import { queryClient } from "@/services/reactQuery";
+import { TooltipProvider } from "ui";
 
 interface ProvidersProps {
   children: ReactNode;
@@ -12,7 +13,9 @@ interface ProvidersProps {
 export function Providers({ children }: ProvidersProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Auth>{children}</Auth>
+      <Auth>
+        <TooltipProvider>{children}</TooltipProvider>
+      </Auth>
     </QueryClientProvider>
   );
 }
