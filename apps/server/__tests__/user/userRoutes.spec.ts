b667d9bab0fa9e6726aa7c48406f97cf2f4d7f07
BrunoRRamos <brbosshd2@gmail.com>
2023-05-28 01:15:38 -0300

    feat: User Routes tests (PUT, DELETE)

--- apps/server/__tests__/user/userRoutes.spec.ts
+++ apps/server/__tests__/user/userRoutes.spec.ts
@@ -7,12 +7,18 @@ const testUserJson = {
     "password":"40028922"
 }
 
+const testUpdateUserJson = {
+    "name":"Tanjiro",
+    "lastName": "Kamado",
+}
+
 const loginUserJson = {
     "email":"gab@gmail.com",
     "password":"40028922"
 }
 
 const resetTest = async () => {
+
     const getInfo = await request(`http://localhost:3001`).post(`/user/login`)
         .send(loginUserJson)
 
@@ -29,7 +35,8 @@ const resetTest = async () => {
 
 
 //Testes de Create
-it("Testa Post de Usuário com Json vazio", async () => {
+it("Testa POST de Usuário com Json vazio", async () => {
+
     const response = await request(`http://localhost:3001`).post(`/user/`)
         .send({})
         .set('Accept', 'application/json')
@@ -38,7 +45,8 @@ it("Testa Post de Usuário com Json vazio", async () => {
         expect(response.status).toBe(500)
 });
 
-it("Testa Post de Usuário", async () => {
+it("Testa POST de Usuário", async () => {
+
     const response = await request(`http://localhost:3001`).post(`/user/`)
         .send(testUserJson)
         .set('Accept', 'application/json')
@@ -47,7 +55,7 @@ it("Testa Post de Usuário", async () => {
         expect(response.status).toBe(201)
 });
 
-it("Testa Post de usuário já cadastrado", async () => {
+it("Testa POST de usuário já cadastrado", async () => {
 
     const duplicate = await request(`http://localhost:3001`).post(`/user/`)
         .send(testUserJson)
@@ -55,25 +63,56 @@ it("Testa Post de usuário já cadastrado", async () => {
         .expect('Content-Type', /json/)
 
         expect(duplicate.status).toBe(409)
-
 });
 
 
 //Testes de GET
-
-it("Testa get de Usuário", async () => {
+it("Testa GET de Usuário", async () => {
 
     const getInfo = await request(`http://localhost:3001`).post(`/user/login`)
-    .send(loginUserJson)
+        .send(loginUserJson)
 
     let userId = getInfo._body.User.id;
     let userToken = getInfo._body.token;
 
     const response = await request(`http://localhost:3001`).get(`/user/${userId}`)
-    .set('Authorization', `Bearer ${userToken}`);
+        .set('Authorization', `Bearer ${userToken}`);
     
-    expect(getInfo.status).toBe(200)
-    expect(response.status).toBe(200)
+        expect(getInfo.status).toBe(200)
+        expect(response.status).toBe(200)
     
-    resetTest();
 });
+
+//Testes de PUT
+it("Testa PUT de usuário", async () => {
+
+    const getInfo = await request(`http://localhost:3001`).post(`/user/login`)
+        .send(loginUserJson)
+
+    let userId = getInfo._body.User.id;
+    let userToken = getInfo._body.token;
+
+    const response = await request(`http://localhost:3001`).put(`/user/${userId}`)
+        .send(testUpdateUserJson)
+        .set('Authorization', `Bearer ${userToken}`);
+
+        expect(response.status).toBe(200)
+});
+
+
+//Testes de DELETE 
+it("Testa DELETE de usuário", async () => {
+
+    const getInfo = await request(`http://localhost:3001`).post(`/user/login`)
+        .send(loginUserJson)
+
+    let userId = getInfo._body.User.id;
+    let userToken = getInfo._body.token;
+
+    const response = await request(`http://localhost:3001`).delete(`/user/${userId}`)
+        .set('Authorization', `Bearer ${userToken}`);
+    
+        expect(getInfo.status).toBe(200)
+        expect(response.status).toBe(201)
+});
+
