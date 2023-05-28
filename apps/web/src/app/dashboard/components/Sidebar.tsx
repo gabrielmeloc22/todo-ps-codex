5afd05137905e1784061570cded99a6831eb5ca4
gabrielmelo22 <gabrielpresb@gmail.com>
2023-05-27 21:58:01 -0300

    feat(web): add responsive variants to the sidebar

--- apps/web/src/app/dashboard/components/Sidebar.tsx
+++ apps/web/src/app/dashboard/components/Sidebar.tsx
@@ -1,11 +1,77 @@
+import { BookmarkIcon, HomeIcon, PlusIcon, UserIcon } from "lucide-react";
+import Link from "next/link";
+import { Button, Separator } from "ui";
 import { Profile } from "./Profile";
 import { CreateTaskDialog } from "./Task/CreateTaskDialog";
 
 export function Sidebar() {
   return (
-    <aside className="w-[20vw] max-w-xs max-2xl:hidden flex flex-col gap-16 px-10 py-24 bg-zinc-800">
-      <Profile />
-      <CreateTaskDialog />
-    </aside>
+    <>
+      <aside className="w-full max-w-[280px] max-xl:hidden flex flex-col gap-8 px-6 py-4 bg-zinc-800">
+        <div className="z-0 flex relative gap-4 items-center w-full pb-4 after:absolute after:left-[-1.5rem] after:top-[-1.5rem] after:w-[calc(100%+3rem)] after:h-[calc(100%+1.5rem)] after:bg-zinc-900/40 after:-z-10">
+          <div className="rounded-full bg-zinc-900/70 w-fit h-fit p-2">✅</div>
+          <h2 className="font-bold">AllDone</h2>
+        </div>
+        <Profile />
+        <div className="mt-10">
+          <CreateTaskDialog
+            trigger={
+              <Button className="w-[90%] px-4" size="lg">
+                Adicionar tarefa <PlusIcon className="ml-2" size={16} />
+              </Button>
+            }
+          />
+        </div>
+      </aside>
+      <aside className="hidden max-xl:flex max-sm:hidden flex-col gap-6 items-center w-24 bg-zinc-800 py-6">
+        <div className="rounded-full bg-zinc-900/70 w-fit h-fit p-2 mb-8">✅</div>
+        <CreateTaskDialog
+          trigger={
+            <Button className="w-fit h-fit p-2" size="lg">
+              <PlusIcon size={20} />
+            </Button>
+          }
+        />
+        <Link href="/dashboard">
+          <Button variant="icon" className="w-fit h-fit p-2 rounded-xl">
+            <HomeIcon size={20} />
+          </Button>
+        </Link>
+        <Link href="/dashboard/profile">
+          <Button variant="icon" className="w-fit h-fit p-2 rounded-xl">
+            <UserIcon size={20} />
+          </Button>
+        </Link>
+        <Link href="/dashboard/collections">
+          <Button variant="icon" className="w-fit h-fit p-2 rounded-xl">
+            <BookmarkIcon size={20} />
+          </Button>
+        </Link>
+      </aside>
+      <aside className="z-10 hidden max-sm:flex gap-10 justify-center p-4 w-full  fixed left-[50%] translate-x-[-50%] bottom-0 mx-auto bg-zinc-800/70 backdrop-blur-md">
+        <CreateTaskDialog
+          trigger={
+            <Button className="w-fit h-fit p-2 rounded-xl" size="lg">
+              <PlusIcon size={24} />
+            </Button>
+          }
+        />
+        <Link href="/dashboard">
+          <Button variant="icon" className="w-fit h-fit p-2 rounded-xl">
+            <HomeIcon />
+          </Button>
+        </Link>
+        <Link href="/dashboard/profile">
+          <Button variant="icon" className="w-fit h-fit p-2 rounded-xl">
+            <UserIcon />
+          </Button>
+        </Link>
+        <Link href="/dashboard/collections">
+          <Button variant="icon" className="w-fit h-fit p-2 rounded-xl">
+            <BookmarkIcon />
+          </Button>
+        </Link>
+      </aside>
+    </>
   );
 }
