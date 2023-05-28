a041bcce839e97cf7193892eb7c204678ac85ff8
Pedro Costa <70077205+PedroCost4@users.noreply.github.com>
2023-05-28 19:49:57 -0300

    Delete create.test.ts

--- apps/server/tests/user/create.test.ts
+++
@@ -1,8 +0,0 @@
-describe('User Create Controller Test', () => {
-    it('throw num1  + num2', () => {
-        const num1 = 2;
-        const num2 = 2;
-
-        expect(num1 + num2).toEqual(4);
-    });
-});
