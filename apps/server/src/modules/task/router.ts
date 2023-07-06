a32563e1023bed8b6818db906bd33d2d20d76434
PedroCost4 <pedro.costacdc@gmail.com>
2023-07-06 19:45:53 -0300

    feat(auth): middleware auth jwt

--- apps/server/src/modules/task/router.ts
+++ apps/server/src/modules/task/router.ts
@@ -1,59 +1,64 @@
 import { z } from "zod";
-import { publicProcedure, router } from "../../trpc";
+import { authenticatedProcedure, publicProcedure, router } from "../../trpc";
 import createTaskUseCase from "./useCases/create";
 import getTaskUseCase from "./useCases/get";
 import DeleteTaskUseCase from "./useCases/delete";
 import UpdateTaskUseCase from "./useCases/update";
 import { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
-
+import CheckUser from "../../utils/checkUser";
 
 export type taskRouterInputs = inferRouterInputs<typeof taskRouter>;
 export type taskRouterOutputs = inferRouterOutputs<typeof taskRouter>;
 
 export const taskRouter = router({
-  createTask: publicProcedure
+  createTask: authenticatedProcedure
     .input(
       z.object({
         title: z.string(),
-        content: z.string().optional(),
-        completionDate: z.string().optional(),
+        content: z.string().optional().nullable(),
+        completionDate: z.string().optional().nullable(),
         status: z.boolean().optional(),
         authorId: z.string(),
-        collectionId: z.string().optional(),
+        collectionId: z.string().optional().nullable(),
       }))
     .mutation(async (opts) => {
-      const { input } = opts;
+      const { input, ctx } = opts;
+      CheckUser.check(ctx.userId, input.authorId);
       const date = createTaskUseCase.stringToDate(input.completionDate);
       return await createTaskUseCase.execute({ ...input, completionDate: date });
     }),
 
-    getTask: publicProcedure
+    getTask: authenticatedProcedure
     .input(z.string())
     .query(async (opts) => {
-      const { input } = opts;
+      const { input, ctx } = opts;
+      CheckUser.check(ctx.userId, input);
       return await getTaskUseCase.execute(input);
     }),
 
-    deleteTask: publicProcedure
+    deleteTask: authenticatedProcedure
     .input(z.string())
     .mutation(async (opts) => {
-      const { input } = opts;
+      const { input, ctx } = opts;
+      CheckUser.check(ctx.userId, input);
       return await DeleteTaskUseCase.execute(input)
     }),
 
-    updateTask: publicProcedure
+    updateTask: authenticatedProcedure
     .input(
       z.object({
         id: z.string(),
         title: z.string().optional(),
         content: z.string().optional(),
         completionDate: z.string().optional(),
+        authorId: z.string(),
         status: z.boolean().optional(),
         collectionId: z.string().optional(),
       })
     ).mutation(async (opts) => {
-      const { input } = opts;
-      const { id: _, ...user } = input;
+      const { input, ctx } = opts;
+      CheckUser.check(ctx.userId, input.authorId);
+      const { id: _, authorId: x, ...user } = input;
       return await UpdateTaskUseCase.execute(input.id, user)
     })
 });
--- apps/server/src/trpc.ts
+++ apps/server/src/trpc.ts
@@ -1,5 +1,6 @@
 import { inferAsyncReturnType, initTRPC } from '@trpc/server';
 import * as trpcExpress from "@trpc/server/adapters/express";
+import { trpcEnsureAuthenticated } from './middleware/trpc.auth';
 
 export const createContext = ({
     req,
@@ -12,5 +13,6 @@ const trpc = initTRPC.context<Context>().create();
 export const router = trpc.router;
 export const middleware = trpc.middleware;
 export const publicProcedure = trpc.procedure;
+export const authenticatedProcedure = trpc.procedure.use(trpcEnsureAuthenticated);
 
 export default trpc;
