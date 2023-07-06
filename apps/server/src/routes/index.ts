1e89d23c6fd919e0ae2926ed8c3248c18138e5b0
PedroCost4 <pedro.costacdc@gmail.com>
2023-07-06 17:36:02 -0300

    fix(auth): fixed auth issues

--- apps/server/src/routes/index.ts
+++ apps/server/src/routes/index.ts
@@ -4,20 +4,21 @@ import collectioRoutes from "../../trash/collection/routes";
 import express from "express";
 import cors from "cors";
 
-import * as trpcExpress from "@trpc/server/adapters/express";
-import { taskRouter } from "../modules/task/router";
-import { createContext } from "../trpc";
+import * as trpcExpress from '@trpc/server/adapters/express'
+import { taskRouter } from "../../../../packages/api/src/routers/task.routes";
+import { createContext } from "../../../../packages/api/src/trpc";
+import { ensureAuthenticated } from "../middleware/ensureAuthenticated";
 
 
 const routes = (app: any) => {
   app
     .use(cors())
     .use(express.json())
+    .use("/api/trpc/task", trpcExpress.createExpressMiddleware({ router: taskRouter, createContext }))
     .use("/user", userRoutes)
-    // .use(ensureAuthenticated)
+    .use(ensureAuthenticated)
     .use("/task", taskRoutes)
     .use("/collection", collectioRoutes)
-    .use("/api/trpc/task", trpcExpress.createExpressMiddleware({ router: taskRouter, createContext }));
 };
 
 export default routes;
