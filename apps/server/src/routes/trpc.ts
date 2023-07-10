e60d798d7e605e8305c2b5d0c013f1b43c81c86c
PedroCost4 <pedro.costacdc@gmail.com>
2023-07-10 17:10:33 -0300

    feat(server):added col router to trpc appRouter

--- apps/server/src/routes/trpc.ts
+++ apps/server/src/routes/trpc.ts
@@ -1,11 +1,13 @@
 import { taskRouter, TaskRouterInputs, TaskRouterOutputs } from "../modules/task/router";
 import { userRouter, UserRouterInputs, UserRouterOutputs } from "../modules/user/router";
+import { collectionRouter, CollectionRouterInputs, CollectionRouterOutputs} from "../modules/collection/router";
 import { router } from "../trpc";
 
 export const appRouter = router({
     task: taskRouter,
     user: userRouter,
+    collection: collectionRouter,
 })
 
 export type AppRouter = typeof appRouter;
-export { TaskRouterInputs, TaskRouterOutputs, UserRouterInputs, UserRouterOutputs };
+export { TaskRouterInputs, TaskRouterOutputs, UserRouterInputs, UserRouterOutputs, CollectionRouterInputs, CollectionRouterOutputs };
