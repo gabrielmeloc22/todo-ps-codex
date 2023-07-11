c2dad44f97cb39a2f318120a8e09bb1ec793d13e
PedroCost4 <pedro.costacdc@gmail.com>
2023-07-11 14:24:18 -0300

    feat: authenticate getUser

--- apps/server/src/modules/user/procedures/get.ts
+++ apps/server/src/modules/user/procedures/get.ts
@@ -2,10 +2,18 @@ import { z } from "zod";
 import { publicProcedure } from "../../../trpc";
 import { trpcEnsureAuthenticated } from "../../../middleware/trpc.auth";
 import GetUserUseCase from "../useCases/get";
+import CheckUser from "../../../utils/checkUser";
 
 const authenticatedProcedure = publicProcedure.use(trpcEnsureAuthenticated);
 
-export const getUserController = authenticatedProcedure.input(z.string()).query(async (opts) => {
-  const { input } = opts;
-  return await GetUserUseCase.execute(input);
-});
+export const getUserController = authenticatedProcedure
+  .input(
+    z.object({
+      id: z.string(),
+    })
+  )
+  .query(async (opts) => {
+    const { input, ctx } = opts;
+    CheckUser.check(ctx.userId, input.id);
+    return await GetUserUseCase.execute(input.id);
+  });
