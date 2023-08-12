023d54e34b2b73924b7c3397587be2e81fd908ae
gabrielmeloc22 <gabrielpresb@gmail.com>
2023-08-11 21:21:54 -0300

    feat(web): setup trpc migration

--- apps/web/src/app/providers.tsx
+++ apps/web/src/app/providers.tsx
@@ -5,6 +5,7 @@ import { ReactNode } from "react";
 import { Auth } from "@/components/Auth";
 import { queryClient } from "@/services/reactQuery";
 import { TooltipProvider } from "ui";
+import { trpc, trpcClient } from "@/services/trpc";
 
 interface ProvidersProps {
   children: ReactNode;
@@ -12,10 +13,12 @@ interface ProvidersProps {
 
 export function Providers({ children }: ProvidersProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Auth>
-        <TooltipProvider>{children}</TooltipProvider>
-      </Auth>
-    </QueryClientProvider>
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <Auth>
+          <TooltipProvider>{children}</TooltipProvider>
+        </Auth>
+      </QueryClientProvider>
+    </trpc.Provider>
   );
 }
